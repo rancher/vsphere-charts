9cc0ecdbf31d5ae81a4da807f467702c08dbfe55
Anna Blendermann <annablender91@gmail.com>
2022-11-08 11:51:00 -0500

    Update CSI and CPI chart image versions (#35)

--- tests/unit/cpi_template_test.go
+++ tests/unit/cpi_template_test.go
@@ -36,7 +36,7 @@ func TestCPITemplateRenderedDaemonset(t *testing.T) {
 				namespace:     "cpitest-" + strings.ToLower(random.UniqueId()),
 				releaseName:   "cpitest-" + strings.ToLower(random.UniqueId()),
 				chartRelPath:  cpiChart,
-				expectedImage: "rancher/mirrored-cloud-provider-vsphere-cpi-release-manager:v1.24.1",
+				expectedImage: "rancher/mirrored-cloud-provider-vsphere-cpi-release-manager:v1.25.0",
 			},
 		},
 		{
@@ -47,7 +47,7 @@ func TestCPITemplateRenderedDaemonset(t *testing.T) {
 				namespace:     "cpitest-" + strings.ToLower(random.UniqueId()),
 				releaseName:   "cpitest-" + strings.ToLower(random.UniqueId()),
 				chartRelPath:  cpiChart,
-				expectedImage: "rancher/mirrored-cloud-provider-vsphere-cpi-release-manager:v1.24.1",
+				expectedImage: "rancher/mirrored-cloud-provider-vsphere-cpi-release-manager:v1.24.2",
 			},
 		},
 		{
