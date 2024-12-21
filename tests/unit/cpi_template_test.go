19eddaecf7bac5d533168e4fe5002b75bd1db96e
Brad Davidson <brad.davidson@rancher.com>
2024-12-21 00:48:27 +0000

    Add Kubernetes v1.32 support
    Note that the CSI does not have any new images or official support for 1.32 yet.
    
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- tests/unit/cpi_template_test.go
+++ tests/unit/cpi_template_test.go
@@ -29,6 +29,17 @@ func TestCPITemplateRenderedDaemonset(t *testing.T) {
 		args args
 	}{
 		{
+			name: "Kubernetes 1.32",
+			args: args{
+				values:        map[string]string{},
+				kubeVersion:   "1.32",
+				namespace:     "cpitest-" + strings.ToLower(random.UniqueId()),
+				releaseName:   "cpitest-" + strings.ToLower(random.UniqueId()),
+				chartRelPath:  cpiChart,
+				expectedImage: "rancher/mirrored-cloud-provider-vsphere:v1.32.1",
+			},
+		},
+		{
 			name: "Kubernetes 1.31",
 			args: args{
 				values:        map[string]string{},
@@ -36,7 +47,7 @@ func TestCPITemplateRenderedDaemonset(t *testing.T) {
 				namespace:     "cpitest-" + strings.ToLower(random.UniqueId()),
 				releaseName:   "cpitest-" + strings.ToLower(random.UniqueId()),
 				chartRelPath:  cpiChart,
-				expectedImage: "rancher/mirrored-cloud-provider-vsphere:v1.31.0",
+				expectedImage: "rancher/mirrored-cloud-provider-vsphere:v1.31.1",
 			},
 		},
 		{
