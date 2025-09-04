e714f7205b3dac18b3b590e564dfc93d8c2d380e
Derek Nola <derek.nola@suse.com>
2025-09-04 13:17:53 -0700

    Add support for k8s v1.34
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- tests/unit/cpi_template_test.go
+++ tests/unit/cpi_template_test.go
@@ -29,6 +29,17 @@ func TestCPITemplateRenderedDaemonset(t *testing.T) {
 		args args
 	}{
 		{
+			name: "Kubernetes 1.34",
+			args: args{
+				values:        map[string]string{},
+				kubeVersion:   "1.34",
+				namespace:     "cpitest-" + strings.ToLower(random.UniqueId()),
+				releaseName:   "cpitest-" + strings.ToLower(random.UniqueId()),
+				chartRelPath:  cpiChart,
+				expectedImage: "rancher/mirrored-cloud-provider-vsphere:v1.34.0",
+			},
+		},
+		{
 			name: "Kubernetes 1.33",
 			args: args{
 				values:        map[string]string{},
